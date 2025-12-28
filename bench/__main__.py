27d8a48f4c66e30f64717c8c22a9fcc101d8f087
Yutian Chen <yutianch@andrew.cmu.edu>
2025-12-28 14:33:51 -0500

    Minor fixes for docker dev-container

---
+++ bench/__main__.py
@@ -0,0 +1,11 @@
+from .SE3_group   import __main__ as SE3_bench
+from .SO3_group   import __main__ as SO3_bench
+from .SO3_algebra import __main__ as so3_bench
+
+def main_bench():
+    SE3_bench.main()
+    SO3_bench.main()
+    so3_bench.main()
+
+if __name__ == "__main__":
+    main_bench()
